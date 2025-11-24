51ec27ae7c21686e10504c453fae8ac7b0b53ac7
brayden-is-tanczos-favorite-student <mertzb@roverkids.org>
2025-11-24 14:07:48 -0500

    Finished 1.4

--- Prototype1/PlayerControl/Assets/Scripts/PlayerControlller.cs
+++ Prototype1/PlayerControl/Assets/Scripts/PlayerControlller.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerControlller : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnspeed;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,8 +15,14 @@ public class PlayerControlller : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {        
+    {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
